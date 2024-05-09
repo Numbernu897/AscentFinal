2ede98eda1762536b559c34934f91c3ea0bcc04c
tuanna-kite <tuanna.kiteacademy@gmail.com>
2024-05-09 09:53:22 +0700

    fix SearchCardItem

--- src/data/data.ts
+++ src/data/data.ts
@@ -32,19 +32,19 @@ export const QUICKSELECT_LIST: QuickSelectItemProps[] = [
     name: "demo",
     image: "https://i.kym-cdn.com/entries/icons/original/000/038/456/christmas_who_wide.png",
     artist: "someone",
-    pin: true
+    pin: true,
   },
   {
     name: "demo",
     image: "https://i.kym-cdn.com/entries/icons/original/000/038/456/christmas_who_wide.png",
     artist: "someone",
-    pin: false
+    pin: false,
   },
   {
     name: "demo",
     image: "https://i.kym-cdn.com/entries/icons/original/000/038/456/christmas_who_wide.png",
     artist: "someone",
-    pin: false
+    pin: false,
   },
 ];
 
@@ -52,38 +52,46 @@ export const TOPMIXES_LIST: TopMixesItemProps[] = [
   {
     name: "demo",
     image: "https://i.kym-cdn.com/entries/icons/original/000/038/456/christmas_who_wide.png",
-    color: "primary.500"
+    color: "primary.500",
   },
   {
     name: "demo",
     image: "https://i.kym-cdn.com/entries/icons/original/000/038/456/christmas_who_wide.png",
-    color: "yellow.200"
+    color: "yellow.200",
   },
   {
     name: "demo",
     image: "https://i.kym-cdn.com/entries/icons/original/000/038/456/christmas_who_wide.png",
-    color: "green.300"
+    color: "green.300",
   },
   {
     name: "demo",
     image: "https://i.kym-cdn.com/entries/icons/original/000/038/456/christmas_who_wide.png",
-    color: "blue.300"
+    color: "blue.300",
   },
   {
     name: "demo",
     image: "https://i.kym-cdn.com/entries/icons/original/000/038/456/christmas_who_wide.png",
-    color: "red.100"
+    color: "red.100",
   },
 ];
-
 
 export const SEARCHCARD_LIST: SearchCardItemProps[] = [
   {
     name: "Demo",
     image: "https://i.kym-cdn.com/entries/icons/original/000/038/456/christmas_who_wide.png",
-    color: "primary.500"
+    color: "primary.500",
   },
-
+  {
+    name: "Demo",
+    image: "https://i.kym-cdn.com/entries/icons/original/000/038/456/christmas_who_wide.png",
+    color: "primary.500",
+  },
+  {
+    name: "Demo",
+    image: "https://i.kym-cdn.com/entries/icons/original/000/038/456/christmas_who_wide.png",
+    color: "primary.500",
+  },
 ];
 
 // export const STATS_LIST: StatsItemProps[] = [
--- src/screens/Explore.tsx
+++ src/screens/Explore.tsx
@@ -1,8 +1,21 @@
 import { Pressable, StyleSheet, View } from "react-native";
 import React from "react";
 import { useAppDispatch } from "../store";
-import { Button, Center, Column, Heading, Row, ScrollView, Box, HStack, Avatar, VStack, Text, IconButton } from "native-base";
-import { Ionicons } from '@expo/vector-icons';
+import {
+  Button,
+  Center,
+  Column,
+  Heading,
+  Row,
+  ScrollView,
+  Box,
+  HStack,
+  Avatar,
+  VStack,
+  Text,
+  IconButton,
+} from "native-base";
+import { Ionicons } from "@expo/vector-icons";
 import SearchCardItem from "../components/SearchCard/SearchCardItem";
 import { SEARCHCARD_LIST } from "../data/data";
 
@@ -15,34 +28,30 @@ const Explore = () => {
           <HStack alignItems="center" space="3">
             {/* <Avatar size="md" source={{ uri: props.avatar }} /> */}
             <VStack>
-              <Heading color="white">
-                Explore
-              </Heading>
+              <Heading color="white">Explore</Heading>
             </VStack>
           </HStack>
         </HStack>
       </Box>
       <Box px="4">
-        {SEARCHCARD_LIST.map((item) => (
-          <SearchCardItem key={item.name + "0"} {...item} />
+        {SEARCHCARD_LIST.map((item, index) => (
+          <SearchCardItem key={item.name + String(index)} {...item} />
         ))}
       </Box>
       <Column my="22" flex={1}>
         <Heading color="white" px="4" mb="4">
           Quick Selection
         </Heading>
-        <ScrollView showsVerticalScrollIndicator={false} >
-          <Column>
-            <Row>
-              {SEARCHCARD_LIST.map((item) => (
-                <SearchCardItem key={item.name + "0"} {...item} />
+        <ScrollView flex={1} showsVerticalScrollIndicator={false}>
+          <Column w="full">
+            <Row w="full" background="red.200">
+              {SEARCHCARD_LIST.map((item, index) => (
+                <SearchCardItem key={item.name + String(index)} {...item} />
               ))}
             </Row>
           </Column>
         </ScrollView>
       </Column>
-
-
     </Column>
   );
 };
